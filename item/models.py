bf871d11394433dd81dc3a296fec166ef204e7b7
Mide Moore <olumide.moore2817@gmail.com>
2023-09-11 09:41:57 +0100

    Added fields for 'created by' and 'created at' and set the display name

--- item/models.py
+++ item/models.py
@@ -19,9 +19,6 @@ class Item(models.Model):
     is_sold=models.BooleanField(default=False)
     #link createdby to user table, related_name is used to get all the items created by the tied user, on_delete is used to delete all the items created by a user when the user is deleted
     created_by=models.ForeignKey(User, related_name='items', on_delete=models.CASCADE) 
-
     created_at=models.DateTimeField(auto_now_add=True) #auto_now_add is used to add the current date and time when the item is created
-    
-
     def __str__(self):
         return self.name
